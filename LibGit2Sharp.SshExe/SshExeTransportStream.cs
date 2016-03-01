9b3882b5bab2d766094236ba99bb1fd288368ac9
Carlos Martín Nieto <carlosmn@github.com>
2016-03-01 13:43:29 +0100

    Better arg passing

--- LibGit2Sharp.SshExe/SshExeTransportStream.cs
+++ LibGit2Sharp.SshExe/SshExeTransportStream.cs
@@ -7,9 +7,6 @@ namespace LibGit2Sharp.SshExe
 {
 	public class SshExeTransportStream : SmartSubtransportStream
 	{
-		readonly SshExeTransport parent;
-		readonly string url;
-
 		Process process;
 
 		bool started;
@@ -33,16 +30,13 @@ namespace LibGit2Sharp.SshExe
         public SshExeTransportStream(SshExeTransport parent, string url, string procName)
             : base(parent)
 		{
-			this.parent = parent;
-			this.url = url;
-
 			// this probably needs more escaping so we pass single quotes
 			// to the upload-pack/receive-pack process itself
             string host, user, path, port;
             splitHostPath(url, out host, out user, out path, out port);
             var args = port == null ?
-                String.Format("'{1}@{0}' '{2}' '{3}'", host, user, procName, path) :
-                String.Format("-p {0} {1}@{0}'{2}' '{3}' '{4}'", port, user, host, procName, path);
+                String.Format("'{3}@{0}' '{1} '\\''{2}'\\'''", host, procName, path, user) :
+                String.Format("-p {4} '{3}@{0}' '{1}' '{2}'", host, procName, path, user, port);
             Console.WriteLine("args {0}", args);
 
             process = new Process();
@@ -53,22 +47,18 @@ namespace LibGit2Sharp.SshExe
             process.StartInfo.RedirectStandardInput = true;
             process.StartInfo.RedirectStandardOutput = true;
             process.ErrorDataReceived += (sender, e) => Console.WriteLine("error: {0}", e.Data);
-		}
+        }
 
 		void AssertAlive()
 		{
             if (!started)
 			{
-                Console.WriteLine("setting error callback");
-                Console.WriteLine("starting ssh");
 				process.Start();
                 started = true;
-                Console.WriteLine("started ssh");
 			}
 
 			if (process.HasExited)
 			{
-                Console.WriteLine("hay error");
                 byte[] buf = new byte[8*1024];
                 int read = process.StandardError.BaseStream.Read(buf, 0, buf.Length);
                 Console.WriteLine("pues {0}", System.Text.Encoding.UTF8.GetString(buf, 0, read));
